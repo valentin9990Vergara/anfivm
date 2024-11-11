02b1ff28d68e214c126bffeaaebf58b44f5391f2
valentin9990Vergara <valentinvergara49@gmail.com>
2024-11-11 14:31:50 -0300

    Home semi completo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,22 @@
 import React from 'react';
-import './Home.css'; // Asegúrate de que la ruta de Home.css esté correcta
+import './Home.css';
 
 function Home() {
     return (
         <div className="home-container">
-            <video 
-                className="background-video"
-                src={`${process.env.PUBLIC_URL}/videos/videoANFI.mp4`}
-                autoPlay
-                loop
-                muted
-            />
+            <video className="background-video" src="/videos/videoANFI.mp4" autoPlay loop muted />
             <div className="home-content">
-                <h1>Bienvenidos a Anfiteatro Villa María</h1>
-                <p>Disfruta de nuestros eventos y servicios.</p>
+                <h1>ANFI Villa María</h1>
+                <p>Bienvenidos a la plataforma oficial de espectáculos de ANFI Villa María.</p>
+            </div>
+
+            {/* Nueva sección debajo de home-content */}
+            <div className="home-description">
+                <p>Enterate de las fechas y todas las noticias de los distintos espectáculos acá.</p>
+                <div className="home-buttons">
+                    <button className="btn-primary">Obtener entradas</button>
+                    <button className="btn-secondary">Saber más</button>
+                </div>
             </div>
         </div>
     );
